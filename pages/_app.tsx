2efde9b956aed38aea4b76afdce5be34fc2bd475
Roman Surkov <s.rb@list.ru>
2022-07-25 19:21:18 +0300

    Рефакторинг

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
+import React from 'react';
 import {AppProps} from "next/dist/shared/lib/router/router";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+    return <>
+        <Head>
+            <title>MyTop - наш лучший топ</title>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <Component {...pageProps}/>
+    </>;
 }
 
 export default MyApp;
