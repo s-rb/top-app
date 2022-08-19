631e87189d763097437d7938454df5676b4ef177
Roman Surkov <s.rb@list.ru>
2022-08-19 20:19:41 +0300

    Анимация сортировки

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -8,11 +8,12 @@ import {declOfNum, priceRu} from "../../helpers/helpers";
 import {Divider} from "../Divider/Divider";
 import Image from "next/image";
 import cn from "classnames";
-import {useRef, useState} from "react";
+import {ForwardedRef, forwardRef, useRef, useState} from "react";
 import {Review} from "../Review/Review";
 import {ReviewForm} from "../ReviewForm/ReviewForm";
+import {motion} from "framer-motion";
 
-export const Product = ({product, className, ...props}: ProductProps): JSX.Element => {
+export const Product = motion(forwardRef((({product, className, ...props}: ProductProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
     const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
     const reviewRef = useRef<HTMLDivElement>(null);
     const scrollToReview = () => {
@@ -24,7 +25,7 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
     };
 
     return (
-        <div className={className} {...props}>
+        <div className={className} {...props} ref={ref}>
             <Card className={styles.product}>
                 <div className={styles.logo}>
                     <Image
@@ -95,4 +96,4 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
             </Card>
         </div>
     );
-}
+})))
--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,7 +1,6 @@
 import {TopPageComponentProps} from "./TopPageComponent.props";
-import {Advantages, Htag, Product, Sort, Tag} from "../../components";
+import {Advantages, HhData, Htag, Product, Sort, Tag} from "../../components";
 import styles from './TopPageComponent.module.css';
-import {HhData} from "../../components";
 import {TopLevelCategory} from "../../interfaces/page.interface";
 import {SortEnum} from "../../components/Sort/Sort.props";
 import {useEffect, useReducer} from "react";
@@ -16,7 +15,7 @@ export const TopPageComponent = ({page, products, firstCategory}: TopPageCompone
 
     // useEffect вызывается каждый раз когда меняется products
     useEffect(() => {
-        dispathSort({ type: 'reset', initialState: products});
+        dispathSort({type: 'reset', initialState: products});
     }, [products]);
 
     return (<div className={styles.wrapper}>
@@ -26,7 +25,8 @@ export const TopPageComponent = ({page, products, firstCategory}: TopPageCompone
                 <Sort sort={sort} setSort={setSort}/>
             </div>
             <div>
-                {sortedProducts && sortedProducts.map(p => (<Product product={p} key={p._id}/>))}
+                {/*Когда указываем layout - значит что компонент при изменении layoutа будет анимировать это изменение*/}
+                {sortedProducts && sortedProducts.map(p => (<Product layout product={p} key={p._id}/>))}
             </div>
             <div className={styles.hhTitle}>
                 <Htag tag={'h2'}>Вакансии - {page.category}</Htag>
