f6187163075651aebfd08a4454ab4fa449203015
Roman Surkov <s.rb@list.ru>
2022-08-24 21:12:20 +0300

    Яндекс метрика добавлена

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,36 @@ import '../styles/globals.css';
 import React from 'react';
 import {AppProps} from "next/dist/shared/lib/router/router";
 import Head from "next/head";
+import ym from 'react-yandex-metrika';
+import {YMInitializer} from 'react-yandex-metrika';
 
 function MyApp({Component, pageProps, router}: AppProps): JSX.Element {
+    // При каждом изменении роута отправляются в яндекс метрику что мы захитились
+    router.events.on('routeChangeComplete', (url: string) => {
+        // Мы не на сервере, а на клиенте
+        if (typeof window !== 'undefined') {
+            ym('hit', url);
+        }
+    })
+
     return <>
         <Head>
             <title>MyTop - наш лучший топ</title>
             <link rel="icon" href="/favicon.ico"/>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link rel="preconnect" href="https://mc.yandex.ru"/>
             <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
                   rel="stylesheet"/>
             <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
             <meta property='og:locale' content="ru_RU"/>
         </Head>
+        {/*Включаем вебвизор и откладываем загрузку метрики до загруэки приложения*/}
+        <YMInitializer
+            accounts={[]}
+            options={{webvisor: true,defer: true}}
+            version='2'
+        />
         <Component {...pageProps}/>
     </>;
 }
